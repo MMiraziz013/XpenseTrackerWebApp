1df9051edf6d4bcc03c9f4f0714f3ee0ce35a260
MMiraziz13==013 <aziz131104@gmail.com>
2025-01-05 19:50:18 +0500

    Addition of the auto-collapsing sidebar, with responsive buttons. General style improvements.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+            
+//const sidebar = document.getElementById('sideBar');
+
+//// Add delay before collapsing the sidebar
+//sidebar.addEventListener('mouseleave', () => {
+//    setTimeout(() => {
+//        sidebar.classList.add('collapsed');
+//    }, 300); // Collapse after 300ms
+//});
+
+//sidebar.addEventListener('mouseenter', () => {
+//    sidebar.classList.remove('collapsed');
+//});
