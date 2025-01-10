b4e2b7c07d3c673cb1de5d14db82a74f8b15408b
MMiraziz13==013 <aziz131104@gmail.com>
2025-01-11 02:21:07 +0500

    Created Category and Transaction models, controllers, CRUD views. Integrated Syncfusion design with license. Updated the design colors. Set up the SQL Server db connection. Made migration and CSS changes/additions.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using XpenseTrackerWebApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
 var app = builder.Build();
+
+// Syncfusion license key
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBPh8sVXJ8S0d+X1JPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9mSX9SdkRjXX5bc3RST2I=;Mgo+DSMBMAY9C3t2XVhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5Sd0ViWXtfdXdVT2Re;MzY2NjIzOEAzMjM4MmUzMDJlMzBHbXJMaENQV2FRYTJ1L3pwNnMyVkJBMmJLYXBsYzFkc2JJWFNDT1NYY2NRPQ==;MzY2NjIzOUAzMjM4MmUzMDJlMzBXYkRURnZiMmhzb2IrckVtaW1HZ1czWVd4R05NOWovNzh6cUZuTnVuYkdNPQ==");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -22,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Category}/{action=Index}/{id?}");
 
 app.Run();
