3db060e48f4152917a50bd0dc2794d33cdbfbb86
MMiraziz13==013 <aziz131104@gmail.com>
2025-01-12 18:39:32 +0500

    New table design. Refactoring of the Category controller. General design update (layout, colors, icons).

--- Models/Category.cs
+++ Models/Category.cs
@@ -17,5 +17,13 @@ namespace XpenseTrackerWebApp.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
