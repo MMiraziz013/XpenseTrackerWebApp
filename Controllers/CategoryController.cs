441a5f582946a3b6f09543ebfbebad1e27896a49
MMiraziz13==013 <aziz131104@gmail.com>
2025-01-13 20:46:14 +0500

    Syncfusion breadcrumb was added.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,24 +24,6 @@ namespace XpenseTrackerWebApp.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/Create
         public IActionResult AddOrEdit(int id=0)
         {
@@ -76,24 +58,6 @@ namespace XpenseTrackerWebApp.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
-        }
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
             return View(category);
         }
 
